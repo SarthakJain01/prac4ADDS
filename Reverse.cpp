ac736d99224f1b796d66084f5b6a95101bc9bd84
SarthakJain01 <88705836+SarthakJain01@users.noreply.github.com>
2022-04-07 00:33:05 +0930

    Add files via upload

--- Reverse.cpp
+++ Reverse.cpp
@@ -3,13 +3,11 @@
 #include "Reverse.h"
 
 using namespace std;
-
 int Reverse::reverseDigit(int value){
 
-int rem = 0;
+static int rem = 0;
 
 rem = (rem*10) + (value%10);
-cout<<rem;
 if (value/10 <= 0)
 {
     return rem;
